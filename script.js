8aea4460ea28122fd64099f1487168c68c64f19f
KenshinTerashima <kenguitar0818@gmail.com>
2026-04-10 19:37:07 +0900

    画像追加

--- script.js
+++ script.js
@@ -3,28 +3,7 @@ const LINKS = {
   x: "https://x.com/kenshin_design_",
 };
 
-const favoriteImageFiles = [
-  "photo_01.png",
-  "photo_02.webp",
-  "photo_03.avif",
-  "photo_04.jpeg",
-  "photo_05.jpg",
-  "photo_06.jpg",
-  "photo_07.jpg",
-  "photo_08.png",
-  "photo_09.jpg",
-  "photo_10.jpg",
-  "photo_11.jpg",
-  "photo_12.png",
-  "photo_13.webp",
-  "photo_14.jpg",
-  "photo_15.jpg",
-  "photo_16.jpg",
-  "photo_17.webp",
-  "photo_18.jpg",
-  "photo_19.jpg",
-  "photo_20.png",
-];
+const favoriteImageFiles = Array.from({ length: 36 }, (_, i) => `photo_${String(i + 1).padStart(2, "0")}.jpg`);
 
 const favoriteImages = favoriteImageFiles.map((file, i) => ({
   id: i + 1,
